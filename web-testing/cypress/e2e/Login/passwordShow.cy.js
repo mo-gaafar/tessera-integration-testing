fa946be0000dd6ccda312953f694862f2609b48c
Reem Yasser <reemyasser332@yahoo.com>
2023-03-20 23:30:25 +0200

    password show/hide

---
+++ web-testing/cypress/e2e/Login/passwordShow.cy.js
@@ -0,0 +1,33 @@
+import Login from "../../pageObjects/loginPage.js";
+
+describe("Login - Normal case", () => {
+  beforeEach("Validate that all elements exist in Login", () => {
+    const loginObj = new Login();
+    loginObj.visitWebsite();
+    loginObj.validateLoginElements();
+  });
+
+  it("Successful Login", () => {
+    const loginObj = new Login();
+    cy.fixture("userIam.json").then((data) => {
+      const loginObj = new Login();
+      loginObj.setEmail(data.email);
+      loginObj.setPassword(data.password);
+    });
+    cy.get(loginObj.passwordInput)
+      .invoke("attr", "type")
+      .should("eq", "password");
+      cy.wait(1000);
+    cy.get(loginObj.passwordShowHide).click();
+    cy.get(loginObj.passwordInput)
+      .invoke("attr", "type")
+      .should("eq", "text");
+      cy.wait(2000);
+      cy.get(loginObj.passwordShowHide).click();
+
+
+    // cy.get(loginObj.navbar).should("not.exist");
+    // cy.get(loginObj.loginBtn).click();
+    // cy.get(loginObj.navbar).should("exist");
+  });
+});
