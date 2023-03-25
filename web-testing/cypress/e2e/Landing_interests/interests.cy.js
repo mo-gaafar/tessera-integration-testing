5df6c44b4558e967836337dcd7e0e34d597fa20d
Reem Yasser <reemyasser332@yahoo.com>
2023-03-25 17:30:07 +0200

    interests updated

--- web-testing/cypress/e2e/Landing_interests/interests.cy.js
+++ web-testing/cypress/e2e/Landing_interests/interests.cy.js
@@ -4,68 +4,58 @@ describe('intersts', () => {
     
   
     it('HomePage', () => {
+      const IN = new intersts();
+      IN.clicksubmit();
+      cy.get(IN.Msg).should("exist").should("have.text", "Nice to meet you!");
+      cy.get(IN.MsgsSubtitle)
+        .should("exist")
+        .should("have.text", "What would you like to do first?");
+      cy.get(IN.CreateBtn).click();
+      cy.wait(5000);
+      cy.url().should("include", "create");
+      cy.go("back");
+      cy.wait(3000);
+      cy.get(IN.AttendBtn).click();
+      cy.wait(5000);
 
-     const IN=new intersts();
-     IN.clicksubmit()
-     cy.get(IN.Msg).should('exist').should('have.text',"Nice to meet you!")
-     cy.get(IN.MsgsSubtitle).should('exist').should('have.text',"What would you like to do first?")
-     cy.get(IN.CreateBtn).click()
-     cy.url().should('include','create')
-     cy.go('back')
-     cy.wait(3000)
-     cy.get(IN.AttendBtn).click()
-     cy.wait(5000)
+      cy.get(IN.MusicsTab).should("exist");
+      cy.get(IN.Alternativebtn).click();
+      cy.get(IN.BusinessTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.FoodTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.CommunityTab)
+        .scrollIntoView({ duration: 1000 })
+        .should("exist");
+      cy.get(IN.ArtTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.FilmsTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.SportsTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.HealthTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.ScienceTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.TravelTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.CharityTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.SpirtualityTab)
+        .scrollIntoView({ duration: 1000 })
+        .should("exist");
+      cy.get(IN.FamilyAndEducation)
+        .scrollIntoView({ duration: 1000 })
+        .should("exist");
+      cy.get(IN.HolidayTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.GovernmentTab)
+        .scrollIntoView({ duration: 1000 })
+        .should("exist");
+      cy.get(IN.FashionTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.TransTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.HobbiesTab).scrollIntoView({ duration: 1000 }).should("exist");
+      cy.get(IN.SchoolActivitiesTab)
+        .scrollIntoView({ duration: 1000 })
+        .should("exist");
+      cy.get(IN.NEXT).click();
+      cy.get(IN.Location).should("exist").type("Al Jizah");
+      cy.get(IN.NEXT).click();
+      cy.get(IN.FollowPage).should("exist");
 
-     cy.get(IN.MusicsTab).should('exist')
-     //cy.get(IN.Alternativebtn).invoke("attr","class").should('include',"interactive")
-     cy.get(IN.Alternativebtn).click()
-     cy.get(IN.Alternativebtn).invoke("attr","class").should('include',"active")
-     cy.get(IN.BusinessTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.FoodTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.CommunityTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.ArtTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.FilmsTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.SportsTab).scrollIntoView({duration:1000}).should('exist') 
-     cy.get(IN.HealthTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.ScienceTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.TravelTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.CharityTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.SpirtualityTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.FamilyAndEducation).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.HolidayTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.GovernmentTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.FashionTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.HomeAndLifestyleTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.TransTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.HobbiesTab).scrollIntoView({duration:1000}).should('exist')
-     cy.get(IN.SchoolActivitiesTab).scrollIntoView({duration:1000}).should('exist')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-        //  cy.visit("https://www.eventbrite.com/signin");
-        //  cy.wait(5000)
-        //  cy.get(home.emailInput).type("samyh0592@gmail.com")
-        //  cy.get(home.passwordInput).type("#7Assansamy")
-        //  cy.get(home.loginBtn).click()
-        //  cy.wait(5000)
-        //  cy.get(home.Logo).should('exist')
-        //  cy.get(home.Likes).should('exist')
-        //  cy.get(home.Tickets).should('exist')
-        //  cy.get("div[data-spec='consumer-header-user-menu'] a[aria-label='Menu dropdown link']").invoke('show').should('be.visible').trigger('mouseover')
-        //  cy.contains('Browse events').click()
-        
+      cy.go('back');
+      cy.go("back");
+      cy.get(IN.Alternativebtn).click();
     })
     
     
--- web-testing/cypress/pageObjects/Interests.js
+++ web-testing/cypress/pageObjects/Interests.js
@@ -1,56 +1,60 @@
-class intersts
-{
-    emailInput = "#email";
-    passwordInput = "#password";
-    loginBtn = ":nth-child(4) > div > .eds-btn";
-    Likes="#heart-chunky_svg__eds-icon--user-chunky_svg"
-    Tickets="div[class='consumer-header__links'] div:nth-child(2) a:nth-child(1)"
-    Msg=".onboarding-reditecion__tittle"
-    MsgsSubtitle=".onboarding-reditecion__subtittle"
-    AttendBox=".onboarding-reditecion__box > :nth-child(1)"
-    AttendBtn=":nth-child(1) > .card_button > .Button_root__1p1bh"
-    CreateBtn=":nth-child(2) > .card_button > .Button_root__1p1bh"
-    CreateBox=".onboarding-reditecion__box > :nth-child(2)"
-    MusicsTab ="#music-note_svg__eds-icon--music-note_svg"
-    BusinessTab ="#seminar_svg__eds-icon--seminar_svg"
-    FoodTab ="#flatware_svg__eds-icon--flatware_svg"
-    CommunityTab ="#park_svg__eds-icon--park_svg"
-    ArtTab="#mask_svg__eds-icon--mask_svg"
-    FilmsTab ="#film-camera_svg__eds-icon--film-camera_base"  
-    SportsTab ="#shoe_svg__eds-icon--sport_base" 
-    HealthTab ="#heart_svg__eds-icon--heart_svg"
-    ScienceTab =".beaker_svg__eds-icon--beaker_base"
-    TravelTab ="#travel_svg__eds-icon--travel_base"
-    CharityTab ="#charity_svg__eds-icon--charity_svg"
-    SpirtualityTab ="#religion_svg__eds-icon--religion_base"
-    FamilyAndEducation ="#rocking-horse_svg__eds-icon--rocking-horse_base"
-    HolidayTab ="#holiday_svg__eds-icon--holiday_svg"
-    GovernmentTab ="#politic_svg__eds-icon--politic_base"
-    FashionTab ="#fashion_svg__eds-icon--fashion_svg"
-    HomeAndLifestyleTab ="#house_svg__eds-icon--house__svg"
-    TransTab =".auto_svg__eds-icon--auto_svg"
-    HobbiesTab ="#game_svg__eds-icon--game_base"
-    SchoolActivitiesTab =".bus_svg__eds-icon--bus_base"
-    Alternativebtn="body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > main:nth-child(1) > div:nth-child(1) > main:nth-child(2) > section:nth-child(1) > section:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(1) > span:nth-child(1)"
-    Classicbtn=":nth-child(1) > .interest-panel__card > .eds-card > .interest-panel__card-contents > :nth-child(4) > .eds-tag > .eds-tag__text"
-    Location="#locationPicker"
-    
-    NEXT="button[class='eds-btn eds-btn--button eds-btn--fill eds-btn--responsive']"
-    FollowPage=".follow-page__empty-state"
+class intersts {
+  emailInput = "#email";
+  passwordInput = "#password";
+  loginBtn = ":nth-child(4) > div > .eds-btn";
+  Likes = "#heart-chunky_svg__eds-icon--user-chunky_svg";
+  Tickets =
+    "div[class='consumer-header__links'] div:nth-child(2) a:nth-child(1)";
+  Msg = ".onboarding-reditecion__tittle";
+  MsgsSubtitle = ".onboarding-reditecion__subtittle";
+  AttendBox = ".onboarding-reditecion__box > :nth-child(1)";
+  AttendBtn = ":nth-child(1) > .card_button > .Button_root__1p1bh";
+  CreateBtn = ":nth-child(2) > .card_button > .Button_root__1p1bh";
+  CreateBox = ".onboarding-reditecion__box > :nth-child(2)";
+  MusicsTab = "#music-note_svg__eds-icon--music-note_svg";
+  BusinessTab = "#seminar_svg__eds-icon--seminar_svg";
+  FoodTab = "#flatware_svg__eds-icon--flatware_svg";
+  CommunityTab = "#park_svg__eds-icon--park_svg";
+  ArtTab = "#mask_svg__eds-icon--mask_svg";
+  FilmsTab = "#film-camera_svg__eds-icon--film-camera_base";
+  SportsTab = "#shoe_svg__eds-icon--sport_base";
+  HealthTab = "#heart_svg__eds-icon--heart_svg";
+  ScienceTab = ".beaker_svg__eds-icon--beaker_base";
+  TravelTab = "#travel_svg__eds-icon--travel_base";
+  CharityTab = "#charity_svg__eds-icon--charity_svg";
+  SpirtualityTab = "#religion_svg__eds-icon--religion_base";
+  FamilyAndEducation = "#rocking-horse_svg__eds-icon--rocking-horse_base";
+  HolidayTab = "#holiday_svg__eds-icon--holiday_svg";
+  GovernmentTab = "#politic_svg__eds-icon--politic_base";
+  FashionTab = "#fashion_svg__eds-icon--fashion_svg";
+  HomeAndLifestyleTab = "#house_svg__eds-icon--house__svg";
+  TransTab = ".auto_svg__eds-icon--auto_svg";
+  HobbiesTab = "#game_svg__eds-icon--game_base";
+  SchoolActivitiesTab = ".bus_svg__eds-icon--bus_base";
+  Alternativebtn =
+    ":nth-child(1) > .interest-panel__card > .eds-card > .interest-panel__card-contents > :nth-child(2) > .eds-tag";
+  ":nth-child(1) > .interest-panel__card > .eds-card > .interest-panel__card-contents > :nth-child(4) > .eds-tag > .eds-tag__text";
+  Location = "#locationPicker";
 
-    clicksubmit()
-    {
-     cy.visit("https://www.eventbrite.com/signin");
-     cy.wait(5000)
-     cy.get(this.emailInput).type("samyh0592@gmail.com")
-     cy.get(this.passwordInput).type("#7Assansamy")
-     cy.get(this.loginBtn).click()
-     cy.wait(5000)
-     
-     cy.get("div[data-spec='consumer-header-user-menu'] a[aria-label='Menu dropdown link']").invoke('show').should('be.visible').trigger('mouseover')
-     cy.contains('Interests').click()
+  NEXT =
+    "button[class='eds-btn eds-btn--button eds-btn--fill eds-btn--responsive']";
+  FollowPage = ".eds-text-hl";
 
-    }
-   
+  clicksubmit() {
+    cy.visit("https://www.eventbrite.com/signin");
+    cy.wait(5000);
+    cy.get(this.emailInput).type("samyh0592@gmail.com");
+    cy.get(this.passwordInput).type("#7Assansamy");
+    cy.get(this.loginBtn).click();
+    cy.wait(5000);
+
+    cy.get(
+      "div[data-spec='consumer-header-user-menu'] a[aria-label='Menu dropdown link']"
+    )
+      .invoke("show")
+      .should("be.visible")
+      .trigger("mouseover");
+    cy.contains("Interests").click();
+  }
 }
 export default intersts
