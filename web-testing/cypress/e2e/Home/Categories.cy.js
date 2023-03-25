7d6f0e62efb193025f24add8901017343aca4c92
Reem Yasser <reemyasser332@yahoo.com>
2023-03-25 16:21:22 +0200

    remove only

--- web-testing/cypress/e2e/Home/Categories.cy.js
+++ web-testing/cypress/e2e/Home/Categories.cy.js
@@ -8,37 +8,37 @@ describe('categories', () => {
      const home=new HomePage();
      
      cy.visit('https://www.eventbrite.com/');
-     cy.wait(5000)
+     cy.wait(2000)
       cy.get(home.MusicIcon).click()
       cy.url().should('include','music')
       cy.go('back')
-      cy.wait(5000)
+      cy.wait(2000)
       cy.get(home.HolidayIcon).click()
       cy.url().should('include','holiday')
       cy.go('back')
-      cy.wait(5000)
+      cy.wait(2000)
      cy.get("#game_svg__eds-icon--game_svg").click()
      
      cy.url().should('include','hobbies')
      cy.go('back')
-     cy.wait(5000)
+     cy.wait(2000)
      cy.get(home.FoodIcon).click()
      cy.url().should('include','food')
      cy.go('back')
-     cy.wait(5000)
+     cy.wait(2000)
 
      cy.get(home.ArtsIcon).click()
      cy.url().should('include','arts')
      cy.go('back')
-     cy.wait(5000)
+     cy.wait(2000)
      cy.get(home.HealthIcon).click()
      cy.url().should('include','health')
      cy.go('back')
-     cy.wait(5000)
+     cy.wait(2000)
      cy.get(home.BusniesIcon).click()
      cy.url().should('include','business')
      cy.go('back')
-     cy.wait(5000)
+     cy.wait(2000)
      cy.get(home.SportsIcon).click()
      cy.url().should('include','sports')
      cy.go('back')
@@ -46,7 +46,7 @@ describe('categories', () => {
          
         
     })
-    it.only("LENGTH",()=>{
+    it("LENGTH",()=>{
         cy.visit('https://www.eventbrite.com/');
         cy.get(".tile--name.eds-text-weight--heavy").should('have.length',8)
     })
