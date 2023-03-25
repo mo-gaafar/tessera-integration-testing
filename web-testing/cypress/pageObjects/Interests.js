cd52361ab4f43efa492aa7655a0770fdae8ba3c0
hassan141 <hassansamy491@gmail.com>
2023-03-25 17:11:17 +0200

    UPDATES

--- web-testing/cypress/pageObjects/Interests.js
+++ web-testing/cypress/pageObjects/Interests.js
@@ -34,9 +34,9 @@ class intersts
     Alternativebtn="body > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > main:nth-child(1) > div:nth-child(1) > main:nth-child(2) > section:nth-child(1) > section:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(1) > span:nth-child(1)"
     Classicbtn=":nth-child(1) > .interest-panel__card > .eds-card > .interest-panel__card-contents > :nth-child(4) > .eds-tag > .eds-tag__text"
     Location="#locationPicker"
-    Giza="#\38 5671047 > .eds-text-list-item > .eds-btn--button"
-    GIZAA="div[class='eds-text-list-item eds-text-list-item--tentatively-selected'] div[class='eds-text-list-item__content-primary']"
-    NEXT=".eds-btn--fill"
+    
+    NEXT="button[class='eds-btn eds-btn--button eds-btn--fill eds-btn--responsive']"
+    FollowPage=".follow-page__empty-state"
 
     clicksubmit()
     {
